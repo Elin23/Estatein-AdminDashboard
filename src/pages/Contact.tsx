9cc1f839a33c8e30334b8880eae801af4fd6927c
elin23 <elinshaia23@gmail.com>
2025-08-09 17:01:29 +0300

    adding Achievements

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -60,11 +60,12 @@
 import { useEffect, useState } from "react";
 import { ref, onValue } from "firebase/database";
 import { db } from "../firebaseConfig";
-import type { Contact } from "../types";
+
 import ContactList from "../components/contact/ContactList";
+import type { ContactType } from "../types";
 
 const Contact = () => {
-  const [contacts, setContacts] = useState<Contact[]>([]);
+  const [contacts, setContacts] = useState<ContactType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -73,7 +74,7 @@ const Contact = () => {
     const unsubscribe = onValue(contactsRef, (snapshot) => {
       const data = snapshot.val();
       if (data) {
-        const list: Contact[] = Object.entries(data).map(([id, value]) => {
+        const list: ContactType[] = Object.entries(data).map(([id, value]) => {
           const val = value as any;
           console.log(data);
           return {
@@ -103,7 +104,7 @@ const Contact = () => {
 
   if (contacts.length === 0) return <div>No contacts found.</div>;
 
-  const handleUpdateStatus = (id: string, status: Contact["status"]) => {
+  const handleUpdateStatus = (id: string, status: ContactType["status"]) => {
     setContacts(
       contacts.map((contact) =>
         contact.id === id ? { ...contact, status } : contact
